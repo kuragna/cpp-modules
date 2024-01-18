ed6a9ac5fa54909f829633607cb8fd27d7068346
kuragna <abourrich@proton.me>
2024-01-18 19:34:00 +0100

    00/ex00: update main

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -4,16 +4,17 @@ int	main(int argc, char **argv)
 {
 	if (argc < 2)
 	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 1;
 	}
-	for (int i = 1; argv[i]; i += 1)
+	for (int i = 1; i < argc; i += 1)
 	{
-		char	*str = argv[i];
-		for (; *str ;)
+		std::string s = argv[i];
+		std::string::iterator it;
+		for (it = s.begin(); it != s.end(); it += 1)
 		{
-			*str = toupper(*str);
-			std::cout << *str;
-			str += 1;
+			*it = std::toupper(*it);
+			std::cout << *it;
 		}
 	}
 	std::cout << std::endl;
